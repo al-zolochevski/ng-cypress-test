d398bf4002c449976763550eeded8e18d417a5f4
al-zolochevski <aleksey.zolochevski@gmail.com>
2022-06-20 12:40:21 +0100

    write test cases using .then and cy.wrap understand the context of using Cypress obj and jQuery obj

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
-  viewportWidth: 1080,
-  viewportHeight: 1920,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,8 +2,32 @@
 
 describe('My first suite', () => {
 
-  it('my first test', () => {
+  it('Understand the differences of ', () => {
     cy.visit('/');
-  })
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
 
+    // Cypress works asynchronous and if you want to save something like variable you should use to.then
+    // When we call .then the jQuery obj is returned in callback function as a parameter
+    cy.contains('nb-card', 'Basic form').then(basicForm => {
+
+      const emailText = basicForm.find('[for="exampleInputEmail1"]').text();
+      expect(emailText).to.equal('Email address');
+
+      const passwordText = basicForm.find('[for="exampleInputPassword1"]').text();
+      expect(passwordText).to.equal('Pass');
+    })
+
+    // And to test something inside .then we should use assertions from Mocha like expect() or should()
+    // And if you want to use Cypress context inside .then you should .wrap returned jQuery obj
+    // And if you want you can use many .then as you wish inside the first .tnen
+    // All variables or constants declared in the first .then will be available in all inside siblings .then
+    cy.contains('nb-card', 'Using the Grid').then(gridForm => {
+
+      let emailText = gridForm.find('[for="inputEmail1"]').text();
+      expect(emailText).to.equal('Email');
+
+      cy.wrap(gridForm).find('[for="inputPassword2"]').should('have.text', 'Password');
+    })
+  })
 });
